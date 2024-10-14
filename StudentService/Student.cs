a9c6c2bc92e02f076c007cab69941e863c11e8d8
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2024-10-14 21:29:07 +0300

    Add project files.

---
+++ StudentService/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentService
+{
+    public class Student : BaseEntity
+    {
+        public string Name { get; set; }
+        private int _age;
+        public string Address { get; init; }
+        public Gender Gender { get; init; }
+        public int Age
+        {
+            set
+            {
+                if(value >= 6 && value <= 21)
+                    _age = value;
+            }
+            get
+            {
+                return _age;
+            }
+        }
+
+        public override string ToString()
+            => $"Id = {Id} \t Name= {Name} \t Age = {_age} \t Address = {Address} \t Gender = {Gender}";
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using StudentService;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student 
+            {
+                Id = 1,
+                Name = "Ahmed",
+                Address = "Cairo",
+                Age = 16,
+                Gender = Gender.Male,
+            };
+
+            Console.WriteLine(student);
+        }
+    }
+}
